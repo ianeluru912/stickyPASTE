4b9364e6975c73acc3a653290e31f70b4d6a16bf
blWizard <mtescuela09@gmail.com>
2024-06-30 11:33:10 -0300

    funcionan correctamente los casos 3 y 4

--- src/lidar.py
+++ src/lidar.py
@@ -13,7 +13,10 @@ class Lidar:
             7:(384,0.05, 0.07), 8:(410,0.012, 0.13), 9:(430,0.09, 0.19), 10:(430,0.055, 0.075), 11:(468,0.055, 0.075), 12:(44,0.055, 0.075), 13:(82,0.055, 0.075),\
             14:(82,0.09, 0.19), 15:(102,0.012, 0.13), 16:(128,0.05, 0.07), 17:(154,0.012, 0.13)}
     
-
+    rays_4 = {4:(277,0.01,0.21), 29:(277,0.046,0.066), 6:(307,0.04,0.24), 5:(307,0.09,0.11), 7:(333,0.133,0.1533), 8:(333,0.086,0.106), 9:(363,0.01,0.21), 30:(363,0.043,0.063), 10:(384,0.049,0.069), 11:(405,0.01,0.21), 31:(405,0.043,0.063),\
+               12:(435,0.086,0.106), 13:(435,0.133,0.1533), 14:(461,0.04,0.24), 15:(461,0.09,0.11), 16:(491,0.01,0.21), 32:(491,0.046,0.066), 17:(0,0.049,0.06), 18:(21,0.01,0.21), 33:(21,0.046,0.066), 19:(51,0.09,0.11), 20:(51,0.04,0.24), 21:(77,0.133,0.1533), \
+                22:(77,0.086,0.106), 23:(107,0.01,0.21), 34:(107,0.043,0.063), 24:(128,0,0), 25:(149,0.01,0.21), 35:(149,0.043,0.063), 26:(179,0.086,0.106), 27:(179,0.133,0.1533), 1:(205,0.09,0.11), 0:(205,0.04,0.24), \
+                    28:(235,0.046,0.066), 2:(235,0.01,0.21), 3:(256,0.049,0.069)}
     
     def __init__(self, lidar, time_step):
         self.lidar = lidar
@@ -64,8 +67,8 @@ class Lidar:
         # gira los rayos para que estén en orientación Norte
         rangeLocal = self.rangeImage[shift:] + self.rangeImage[:shift]
         walls = {}
-        for i in self.rays_3.keys():
-            walls[i]=(rangeLocal[self.rays_3[i][0]])
+        for i in self.rays_4.keys():
+            walls[i]=(rangeLocal[self.rays_4[i][0]])
         return walls
 
     def get_walls_1(self, rotation):
@@ -183,6 +186,77 @@ class Lidar:
             walls[14]=-1
             walls[15]=-1
             walls[16]=-1
+        return walls
+    
+    def get_walls_4(self, rotation):
+        shift = self.rotToLidar(rotation)
+        rangeLocal = self.rangeImage[shift:] + self.rangeImage[:shift]
+        walls = {}
+        for i in self.rays_4.keys():
+            ray=self.rays_4[i][0]
+            rayDistance=rangeLocal[ray]
+            lowerLimit=self.rays_4[i][1]
+            upperLimit=self.rays_4[i][2]
+            if rayDistance>=lowerLimit and rayDistance<=upperLimit:
+                walls[i]= 1
+            elif rayDistance<lowerLimit:
+                walls[i]= -1
+            else:
+                walls[i]= 0
+        if walls[1]==1:
+            walls[0]=-1
+        if walls[28]==1:
+            walls[0]=-1
+            walls[1]=-1
+            walls[2]=-1
+            walls[3]=-1
+        if walls[29]==1:
+            walls[3]=-1
+            walls[4]=-1
+            walls[5]=-1
+            walls[6]=-1
+        if walls[30]==1:
+            walls[10]=-1
+            walls[7]=-1
+            walls[8]=-1
+            walls[9]=-1
+        if walls[31]==1:
+            walls[10]=-1
+            walls[11]=-1
+            walls[12]=-1
+            walls[13]=-1
+        if walls[32]==1:
+            walls[16]=-1
+            walls[17]=-1
+            walls[14]=-1
+            walls[15]=-1
+        if walls[33]==1:
+            walls[17]=-1
+            walls[18]=-1
+            walls[19]=-1
+            walls[20]=-1
+        if walls[34]==1:
+            walls[21]=-1
+            walls[22]=-1
+            walls[23]=-1
+            walls[24]=-1
+        if walls[35]==1:
+            walls[24]=-1
+            walls[25]=-1
+            walls[26]=-1
+            walls[27]=-1
+        if walls[8]==1:
+            walls[7]=-1
+        if walls[12]==1:
+            walls[13]=-1
+        if walls[15]==1:
+            walls[14]=-1
+        if walls[19]==1:
+            walls[20]=-1
+        if walls[22]==1:
+            walls[21]=-1
+        if walls[26]==1:
+            walls[27]=-1
         return walls
 
     def rotToLidar(self, rot):
--- src/main.py
+++ src/main.py
@@ -9,44 +9,44 @@ robot = Robot()
 
 mapvis = MapVisualizer()
 
-# while robot.step() != -1:
-    # # 0. Actualizar mapa
-    # robot.updateMap()
-    # robot.map.writeMap("map.txt", robot)
-    # mapvis.send_map(robot.map)    
+while robot.step() != -1:
+    # 0. Actualizar mapa
+    robot.updateMap()
+    robot.map.writeMap("map.txt", robot)
+    mapvis.send_map(robot.map)    
 
-    # # 1. Dame baldosas vecinas candidatas a donde puedo moverme
-    # tiles = robot.checkNeighbours()
+    # 1. Dame baldosas vecinas candidatas a donde puedo moverme
+    tiles = robot.checkNeighbours()
     
-    # # 2. Elegir una baldosa de las candidatas
-    # tiles.sort(key=lambda t: t.visits)
-    # tile = tiles[0]
+    # 2. Elegir una baldosa de las candidatas
+    tiles.sort(key=lambda t: t.visits)
+    tile = tiles[0]
 
-    # # 3. Mover robot a baldosa elegida
-    # robot.moveToTile(tile)
-    # print(robot.bh_ahead())
-    # print(robot.imageProcessor.see_hole(robot.convertir_camara(robot.camD.getImage(), 64, 64)))
-    # # 4. Repetir
+    # 3. Mover robot a baldosa elegida
+    robot.moveToTile(tile)
+    print(robot.bh_ahead())
+    print(robot.imageProcessor.see_hole(robot.convertir_camara(robot.camD.getImage(), 64, 64)))
+    # 4. Repetir
     
-print(robot.position)
-destino=Point(robot.position.x,robot.position.y+0.06)
-robot.moveToPoint(destino)
-print(robot.position)
-print(robot.lidar.get_walls_2(robot.rotation))
-print('---')
-destino=Point(robot.position.x,robot.position.y+0.18)
-robot.moveToPoint(destino)
-print(robot.position)
-print(robot.lidar.get_walls_2(robot.rotation))
-print('---')
-destino=Point(robot.position.x+0.18,robot.position.y)
-robot.moveToPoint(destino)
-destino=Point(robot.position.x,robot.position.y-0.12)
-robot.moveToPoint(destino)
-print(robot.position)
-print(robot.lidar.ver_walls(robot.rotation))
-print(robot.lidar.get_walls_3(robot.rotation))
-print('---')
+# print(robot.position)
+# destino=Point(robot.position.x,robot.position.y+0.06)
+# robot.moveToPoint(destino)
+# print(robot.position)
+# print(robot.lidar.get_walls_2(robot.rotation))
+# print('---')
+# destino=Point(robot.position.x,robot.position.y+0.18)
+# robot.moveToPoint(destino)
+# print(robot.position)
+# print(robot.lidar.get_walls_2(robot.rotation))
+# print('---')
+# destino=Point(robot.position.x+0.18,robot.position.y)
+# robot.moveToPoint(destino)
+# destino=Point(robot.position.x,robot.position.y-0.12)
+# robot.moveToPoint(destino)
+# print(robot.position)
+    # print(robot.lidar.ver_walls(robot.rotation))
+    # print(robot.lidar.get_walls_4(robot.rotation))
+    # print('---')
     # print(robot.lidar.ver_walls(robot.rotation))
     # print('---')
 
